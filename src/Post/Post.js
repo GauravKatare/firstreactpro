a74bb2af519b12be4bed2d49533935a421596cfe
GauravKatare <kataregaurav1501@gmail.com>
2020-07-12 02:54:40 +0530

    get is working

---
+++ src/Post/Post.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const Post =(props) => {
+   
+    return (
+        <div>
+            <p>{props.title}</p>
+        </div>
+            //<input type="text" onChange={this.props.changes} value={this.props.name}></input>
+    );
+}
+
+export default Post;
--- src/UserInput/UserInput.js
+++ src/UserInput/UserInput.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
+import Post from '../Post/Post.js';
 
 class UserInput extends Component{
+
+    state={
+        posts:[]
+    }
+    componentDidMount(){
+        axios.get('https://jsonplaceholder.typicode.com/posts').then(
+            response =>{
+                this.setState({posts:response.data});
+                console.log(response);
+            }
+        );
+    }
     render(){
+        const posts=this.state.posts.map(post=> {
+            return <Post title={post.title} />
+        });
+
         return (
          
-            <div></div>
+        <div>{posts}</div>
                 //<input type="text" onChange={this.props.changes} value={this.props.name}></input>
          
         );
